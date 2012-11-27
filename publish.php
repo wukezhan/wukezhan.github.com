9fb60ec9253f8543f7707f514b96c97560291282
wukezhan <wukezhan@gmail.com>
2012-11-27 16:00:40 +0800

    ...

--- publish.php
+++ publish.php
@@ -13,10 +13,12 @@ class Git
     protected $_changes = array(
         'add' => array(),
         'modified' => array(),
-        'deleted' => array()
+        'deleted' => array(),
+        'new file' => array()
     );
     public function execute($cmd)
     {
+        
         ob_start();
         system($cmd, $ret);
         $ret = ob_get_contents();
@@ -40,7 +42,7 @@ class Git
                 $current = $signs[$line];
             }else{
                 if(preg_match('/^#([\t]+)([^\(]+)/', $line, $matches)){
-                    if (preg_match('/(modified|deleted):[\t\s]+([^\n]+)/', $matches[2], $matches2)){
+                    if (preg_match('/(modified|deleted|new file):[\t\s]+([^\n]+)/', $matches[2], $matches2)){
                         array_push($this->_changes[$matches2[1]], $matches2[2]);
                     }else{
                         array_push($this->_changes['add'], $matches[2]);
@@ -56,21 +58,28 @@ class Git
             'modified' => 'add',
             'deleted' => 'rm'
         );
-        $adds = array_merge($this->_changes['add'], $this->_changes['modified']);
+        $adds = array_merge($this->_changes['add'], $this->_changes['modified'], $this->_changes['new file']);
         $rms = $this->_changes['deleted'];
         
+        $adds = array_unique($adds);
+        $rms = array_unique($rms);
         $this->add($adds);
         $this->rm($rms);
     }
     public function add($files)
     {
-        $cmd = "git add '".implode("' '", $files)."'";
-        $this->execute($cmd);
+        if ($files){
+            echo $cmd = "git add '".implode("' '", $files)."'";
+            $this->execute($cmd);
+        }
     }
     public function rm($files)
     {
-        $cmd = "git rm '".implode("' '", $files)."'";
-        $this->execute($cmd);
+        if ($files){
+            echo $cmd = "git rm -r '".implode("' '", $files)."'";
+            //$this->execute($cmd);
+        }
+        
     }
     public function commit($files, $msg)
     {
@@ -85,5 +94,6 @@ class Git
 $git = new Git();
 $git->status();
 $git->prepare();
+//var_dump($git);
 $git->commit('.', '...');
 
